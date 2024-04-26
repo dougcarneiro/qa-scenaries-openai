e2828ef1c2c8a2ace75d4218940100f8391ea472
Douglas Carneiro <douglasefcarneiro@gmail.com>
2024-04-26 14:07:38 -0300

    Add: adding user and assistent samples for more contexto; moving context files to separated folder

--- script.py
+++ script.py
@@ -8,25 +8,34 @@ from openai import OpenAI
 
 dotenv.load_dotenv()
 
-SYSTEM_CONTEXT_PATH = 'system_context.txt'
+SYSTEM_CONTEXT_PATH = 'context/system_context.txt'
+ASSISTENT_SAMPLE_PATH = 'context/assistant_output_sample.txt'
+USER_SAMPLE_PATH = 'context/user_input_sample.txt'
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Script para gerar cenários de testes utilizando a IA da OpeinAI')
-    parser.add_argument('--input_file', '-i', type=str, help='Caminho do arquivo de input contendo os detalhes da task')
-    parser.add_argument('--output_dir', '-o', type=str, default='./', help='Caminho de destino do diretório')
+    parser = argparse.ArgumentParser(
+        description='Script para gerar cenários de testes utilizando a IA da OpeinAI')
+    parser.add_argument('--input_file', '-i', type=str,
+                        help='Caminho do arquivo de input contendo os detalhes da task')
+    parser.add_argument('--output_dir', '-o', type=str,
+                        default='./', help='Caminho de destino do diretório')
 
     return parser.parse_args()
 
 
-def request(system_context, input_context):
+def request(system_context, input_context, user_sample, assistant_sample):
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
     response = client.chat.completions.create(
-      model="gpt-4-turbo",
-      messages=[
-        {"role": "system", "content": system_context},
-        {"role": "user", "content": input_context},
-      ]
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": system_context},
+            {"role": "user", "content": user_sample},
+            {"role": "assistant", "content": assistant_sample},
+            {"role": "user", "content": input_context}
+        ]
     )
+    print(response.usage)
     return response.choices[0].message.content
 
 
@@ -41,7 +50,8 @@ def main():
     output_dir = args.output_dir[:-1] if args.output_dir.endswith('/') else args.output_dir
 
     if not user_input:
-        warnings.warn("Você precisa definir um arquivo de entrada com '--input_file [path]'")
+        warnings.warn(
+            "Você precisa definir um arquivo de entrada com '--input_file [path]'")
         return
 
     with open(SYSTEM_CONTEXT_PATH) as system_file:
@@ -51,7 +61,16 @@ def main():
         task_title = input_file.readline().strip()
         input_context = input_file.read()
 
-    md_content = request(system_context, input_context)
+    with open(USER_SAMPLE_PATH) as user_sample_file:
+        user_sample_content = user_sample_file.read()
+
+    with open(ASSISTENT_SAMPLE_PATH) as assistant_sample_file:
+        assistant_sample_content = assistant_sample_file.read()
+
+    md_content = request(system_context,
+                         input_context,
+                         assistant_sample_content,
+                         user_sample_content)
     write_md(md_content, task_title, output_dir)
 
 
